93a534f2f53fcecbc79556c804beb3417712e0a7
Chun-Chung Yang <chunchung.yang@gmail.com>
2015-12-07 03:03:35 -0500

    slide bar
    2014-2011 data is ready

--- public_html/js/maps.js
+++ public_html/js/maps.js
@@ -13,7 +13,7 @@ var g = svg.append("g").attr("id", "states");
 
 var vertex_color = d3.interpolateHsl("#FF0000", "#00FF00");
 
-queue()
+var map_explorer = queue()
 	.defer(d3.json, "data/states_usa.topo.json")
 	.defer(d3.json, "data/us-state-centroids.json")
 	.defer(d3.json, "data/max-min-by-year.json")
@@ -29,16 +29,18 @@ queue()
 	.defer(d3.json, "data/state-data-by-year-2014.json")
 	.await(ready);
 
-	function ready(error, us, centroid, year_range,us_2005, us_2006, us_2007,us_2008, us_2009, us_2010,us_2011,us_2012,us_2013,us_2014) {
+var explorer={};
+var year_range,us_data, centroid_data, countries, neighbors, dataset, year;
+
+function ready(error, us, centroid, yr ,us_2005, us_2006, us_2007,us_2008, us_2009, us_2010,us_2011,us_2012,us_2013,us_2014) {
 		state = null;
 		if (error) throw error;
 
-		var countries = topojson.feature(us, us.objects.states).features,
-			neighbors = topojson.neighbors(us.objects.states.geometries);
+		countries = topojson.feature(us, us.objects.states).features;
+		neighbors = topojson.neighbors(us.objects.states.geometries);
 
-		var year = "2014";
-
-		var dataset = {
+		year_range = yr;
+		dataset = {
 			"2005": us_2005, 
 			"2006": us_2006,
 			"2007": us_2007,
@@ -51,38 +53,45 @@ queue()
 			"2014": us_2014,
 		};
 
-		console.log(dataset);
+		us_data = us;
+		centroid_data = centroid;
+		explorer.init();
+		return explorer.update(2014);                                      
+}
 
-
+explorer.init = function() {
 		g.selectAll("states")
 			.data(countries)
 			.enter().insert("path", ".graticule")
 			.attr("class", "states")
 			.attr("d", path)
 			.style("fill", function(d, i) { return "#CCC"; })
-			.style("fill-opacity", function(d){ console.log(dataset["2014"][d.properties.name]) ;return 5*dataset["2014"][d.properties.name]["poverty-population"]/dataset["2014"][d.properties.name]["population"];})
+			.style("fill-opacity", function(d){return 5*dataset["2014"][d.properties.name]["poverty-population"]/dataset["2014"][d.properties.name]["population"];})
 			.on('mouseover', function(d, i) {
 				var currentState = this;
 				d3.select(this).style('fill-opacity', .7);
 
 			})
 		.on('mouseout', function(d, i) {
+			//console.log(d.properties.name);
 			d3.selectAll('path')
-			.style("fill-opacity", function(d){ console.log(dataset["2014"][d.properties.name]) ;return 5*dataset["2014"][d.properties.name]["poverty-population"]/dataset["2014"][d.properties.name]["population"];});
+			.style("fill-opacity", function(d){ console.log(d); return 5*dataset["2014"][d.properties.name]["poverty-population"]/dataset["2014"][d.properties.name]["population"];});
 		})
 		.on('click', state_clicked);
+		return explorer;
+	}
+
+explorer.update = function(_year) {
 
 
 		var radius = d3.scale.sqrt()
 			.domain([0, 1e6])
 			.range([0, 10]);
 
+		g.selectAll(".vertex").remove();
 
-		console.log(centroid);
-
-
-		var glyph =g.selectAll(".vertex")
-			.data(centroid.features)                  
+		var glyph = g.selectAll(".vertex")
+			.data(centroid_data.features)                  
 			.enter()                       
 			.append("g")                   
 			.attr("id", function(d) {      
@@ -90,28 +99,32 @@ queue()
 			.attr("class", "vertex")       
 			.style("opacity", "1");        
 
+
+		console.log(_year);
+
 			//.data(centroid.features.sort(function(a, b) { return b.properties.population - a.properties.population; }))
 		glyph.append("circle")
 			.attr("class", "symbol")
 			//.attr("d", path.pointRadius(function(d) { return radius(d.properties.population); }));
-			.attr("cx", function(d) {console.log(d.properties.name); console.log(dataset["2014"][d.properties.name]);return path.centroid(d)[0]})
+			.attr("cx", function(d) {return path.centroid(d)[0]})
 			.attr("cy", function(d) {return path.centroid(d)[1]})
-			.attr("r", function(d) {return 0.7*radius(dataset["2014"][d.properties.name]["population"]); })
+			.attr("r", function(d) {return 0.7*radius(dataset[_year][d.properties.name]["population"]); })
 			.style("fill", function(d) {
-				    return vertex_color((dataset["2014"][d.properties.name]["GDP"] - year_range["2014"]["min_GDP"])/(year_range["2014"]["max_GDP"]-year_range["2014"]["min_GDP"]));
+				    return vertex_color((dataset[_year][d.properties.name]["GDP"] - year_range[_year]["min_GDP"])/(year_range[_year]["max_GDP"]-year_range[_year]["min_GDP"]));
 			});
                                                                      
-		centroid.features.forEach(function(d)                                            
+		centroid_data.features.forEach(function(d)                                            
 				{                                                                     
 					var cur_state = d.properties.name;
                                                          
 					var d2 = [                                                        
 			[                                                                 
-		{axis:"income",value:(dataset["2014"][cur_state]["income"] - year_range["2014"]["min_income"])/(year_range["2014"]["max_income"]-year_range["2014"]["min_income"])},                                          
-		{axis:"house value",value:(dataset["2014"][cur_state]["median-house-value"] - year_range["2014"]["min_house"])/(year_range["2014"]["max_house"]-year_range["2014"]["min_house"])},                                          
-		{axis:"expenditure",value:(dataset["2014"][cur_state]["pce"] - year_range["2014"]["min_pce"])/(year_range["2014"]["max_pce"]-year_range["2014"]["min_pce"])}                                         
+		{axis:"income",value:(dataset[_year][cur_state]["income"] - year_range[_year]["min_income"])/(year_range[_year]["max_income"]-year_range[_year]["min_income"])},                                          
+		{axis:"house value",value:(dataset[_year][cur_state]["median-house-value"] - year_range[_year]["min_house"])/(year_range[_year]["max_house"]-year_range[_year]["min_house"])},                                          
+		{axis:"expenditure",value:(dataset[_year][cur_state]["pce"] - year_range[_year]["min_pce"])/(year_range[_year]["max_pce"]-year_range[_year]["min_pce"])}                                         
 			]                                                                 
 			];                                                                
+
 
 		//Options for the Radar chart, other than default                 
 		var mycfg = {                                                     
@@ -123,8 +136,13 @@ queue()
                                                  
 		RadarChart.draw("#" + cur_state.split(/[ ,]+/).join('_'), d2, mycfg, [path.centroid(d)[0]-radius(d.properties.population), path.centroid(d)[1]-radius(d.properties.population)]);     
 				}                                                                     
-		);                                                                    
+		);       
+
+		return explorer;
 }
+
+
+                    
 
 function get_xyz(d) {
 	var bounds = path.bounds(d);
